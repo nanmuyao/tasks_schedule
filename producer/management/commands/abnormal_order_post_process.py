b5b68e656a5c28ae87ebcf31f56e76b64bc0f5b5
hanzhiqiang <1558249222han@gmail.com>
2021-02-04 18:40:33 +0800

    add producer

---
+++ producer/management/commands/abnormal_order_post_process.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
